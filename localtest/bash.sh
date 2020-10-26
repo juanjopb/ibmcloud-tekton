d74665c1cca5fe43c73d3c601688eaecd875dbe2
JuanJosePerez <juanjose.perez@geniussportsmedia.com>
2020-10-26 14:59:20 -0500

    Changes that works on Dockerfile

--- localtest/bash.sh
+++ localtest/bash.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-yum install skopeo vim -y
+#yum install skopeo vim -y
+yum install pinentry -y
 cat >signsample <<EOF
-      %echo Generating a basic OpenPGP key
+      #%echo Generating a basic OpenPGP key
+      %no-protection
       Key-Type: DSA
       Key-Length: 1024
       Subkey-Type: ELG-E
@@ -16,20 +18,52 @@ cat >signsample <<EOF
       %commit
       %echo done
 EOF
-gpg2 --batch --gen-key signsample
-gpg2 --list-secret-keys
-gpg2 --no-default-keyring --secret-keyring ./signsample.sec --keyring ./signsample.pub --list-secret-keys
-APP_IMAGE="$(params.image-server)/$(params.image-namespace)/$(params.image-repository):$(params.image-tag)"
-APP_RELEASE="$(params.image-release)"
-NAME_IMAGE="$(params.image-repository):$(params.image-tag)"
-buildah --layers --storage-driver=$(params.STORAGE_DRIVER) bud --format=$(params.FORMAT) --tls-verify=$(params.TLSVERIFY) -f $(params.DOCKERFILE) -t ${APP_IMAGE} $(params.CONTEXT)
-set +x
-if [[ -n "${REGISTRY_USER}" ]] && [[ -n "${REGISTRY_PASSWORD}" ]] && [[ "$(params.image-server)" != "image-registry.openshift-image-registry.svc:5000"  ]]; then
-  buildah login -u "${REGISTRY_USER}" -p "${REGISTRY_PASSWORD}" "$(params.image-server)"
-  echo "buildah login -u "${REGISTRY_USER}" -p "xxxxx" "$(params.image-server)""
-fi
-set -x
-buildah --storage-driver=$(params.STORAGE_DRIVER) push --tls-verify=$(params.TLSVERIFY) --digestfile ./image-digest ${APP_IMAGE} docker://${APP_IMAGE}
-skopeo inspect --tls-verify=false docker://${APP_IMAGE}
-sed -i "s/var\/lib\/containers\/sigstore/tekton\/home\/.gnupg\//g" /etc/containers/registries.d/default.yaml
-skopeo --tls-verify=false --insecure-policy --debug copy --sign-by signsample@foo.bar docker://${APP_IMAGE} docker://${APP_IMAGE}.signed
+PASSPHRASE=abc
+export GPG_TTY=$(tty)
+echo "*****Create gpg-agent.conf"
+ls /usr/bin/pinentry-curses
+echo ‘pinentry-program /usr/bin/pinentry-curses’ > ~/.gnupg/gpg-agent.conf
+cat >/etc/gnupg/gpg-agent.conf <<EOF
+      pinentry-program /usr/bin/pinentry-curses
+      allow-loopback-pinentry
+EOF
+echo "*****Create gpg.conf"
+cat >~/.gnupg/gpg.conf <<EOF
+      use-agent 
+      pinentry-mode loopback
+EOF
+echo "********* Located on ******"
+pwd
+gpg --batch -v --gen-key signsample
+echo "*********Firts List Keys"
+gpg --list-keys
+echo "*********Import the Public GPG to Keystore"
+
+gpg --import signsample.pub
+echo "*********List Secret Keys"
+gpg --list-secret-keys
+echo "*********List Keys"
+gpg --list-keys
+echo "*********Make sigstore"
+mkdir -p /source/sigstore/
+chmod 777 /source/sigstore/
+echo "*********Make sigstore "
+echo "*********Change Path Sigstore"
+sed -i "s/var\/lib\/containers\/sigstore/source\/sigstore\//g" /etc/containers/registries.d/default.yaml
+echo "*********Make sigstore"
+cat /etc/containers/registries.d/default.yaml
+ls -lsrt ~/.gnupg/
+echo "*********Export public key"
+gpg --armor --export signsample@foo.bar > sign-sample.pub
+SIGN_CA=$(cat sign-sample.pub)
+echo "*********PRINT public key"
+echo "${SIGN_CA}"
+cat ~/.gnupg/gpg.conf
+
+echo "*****REstart Agent"
+#echo RELOADAGENT | gpg-connect-agent
+echo "*****SKOPEO DOCKER with Sign and passhprase"
+echo "********************************************"
+echo "********************************************"
+#--registries.d=/source/sigstore/
+skopeo copy --debug --dest-creds=juanjosepb:Pillin52 --insecure-policy --src-tls-verify=false --dest-tls-verify=false --sign-by signsample@foo.bar docker://juanjosepb/nginx:1.0 docker://docker.io/juanjosepb/signedimage:3.0.signed <<<$PASSPHRASE
